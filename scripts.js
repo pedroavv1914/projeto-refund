17cab35b09159fc8b5ef955596b3bde3e631992b
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-06-16 19:54:17 -0300

    Formatting the currency

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
-// SELECIONA OS ELEMENTOS DO FORMULÁRIO
+// SELECIONA OS ELEMENTOS DO FORMULÁRIO.
 const amount = document.getElementById("amount")
 
+// CAPTURA O EVENTO DE IMPUT PARA FORMATAR O VALOR EM APENAS NUMEROS.
 amount.oninput = () => {
     let value = amount.value.replace(/\D/g, "")
 
-    amount.value = value
+    //TRANSFORMA O VALOR EM CENTAVOS.
+    value = Number(value) / 100
+
+    // ATUALIZA O VALOR DO INPUT.
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+
+    // FORMATA O VALOR NO PADRÃO BRL (REAL BRASILEIRO)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return value
 }
